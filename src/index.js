e110a6a31903f67a2d377030a2756bcd84c015af
John Crisostomo <johncrisostomo@protonmail.ch>
2018-03-19 15:11:17 +0800

    Move React code to index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-require("./index2.js");
-console.log("Hello from index.js");
+const React = require("react");
+const ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  React.createElement("h1", false, "Hello from React!"),
+  document.getElementById("root")
+);
